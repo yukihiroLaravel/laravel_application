3c8702852bbc4fd07e31223389a17d0191a6072d
SekiYukihiro <hiroyuki.seki.email@gmail.com>
2022-06-11 10:55:10 +0900

    4章 各動画のいいね数

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -6,6 +6,12 @@
         @endif
             <div class="col-lg-4 mb-5">
                 <div class="movie text-left d-inline-block">
+                    @php
+                        $countFavoriteUsers = $movie->favoriteUsers()->count();
+                    @endphp
+                    <div class="text-right mb-2">いいね！
+                        <span class="badge badge-pill badge-success">{{ $countFavoriteUsers }}</span>
+                    </div>
                     <div>
                         @if ($movie)
                             <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}" frameborder="0"></iframe>
