87148a3fb769b97e62eb0f60badab3afe0b89cd4
tokushige <toku.s.works@gmail.com>
2022-08-21 22:24:01 +0900

    4章　動画マイグレーションとモデル

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::all();
+
+        return view('welcome', ['user' => $users]);
     }
 }
