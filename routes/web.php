f3680511cbe8c49443c11c45b334a5a261bbf9eb
daisuke <bucchi_orange@yahoo.co.jp>
2022-09-04 10:22:53 +0900

    ４章　ユーザ一覧、動画投稿・削除

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,15 @@ Route::get('logout','Auth\LoginController@logout')->name('logout');
 
 Route::get('/','UsersController@index');
 
+//ログイン後
+Route::group(['middleware' =>'auth'],function(){
+    //動画
+    Route::prefix('movies')->group(function(){
+        Route::get('crate','MoviesController@create')->name
+        ('movie.create');
+        Route::post('','MoviesController@store')->name
+        ('movie.store');
+        Route::delete('{id}','MoviesController@destroy')->name
+        ('movie.delete');
+    });
+});
