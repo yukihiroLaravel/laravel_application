8e5576c175f7949ca5fce80965c45d0bdd7d6c20
yusuke okada <y.okada.2125@gmail.com>
2022-11-16 22:24:27 +0900

    4章 動画登録・ユーザ詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,9 +10,21 @@ class UsersController extends Controller
     public function index()
     {
         $users = User::orderBy('id', 'desc')->paginate(9);
-        
+
         return view('welcome', [
             'users' => $users,
         ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data = [
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
     }
 }
