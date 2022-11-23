e5af462b251243f34640880c2688f4f1f427c909
yusuke okada <y.okada.2125@gmail.com>
2022-11-23 23:28:04 +0900

    5章 いいね！機能・カラム追加

--- routes/web.php
+++ routes/web.php
@@ -14,16 +14,19 @@
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 // ログイン
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/', 'UsersController@index');
-Route::prefix('users')->group(function () {
-    Route::get('{id}', 'UsersController@show')->name('user.show');
-});
+// ユーザ
+Route::get('/', 'UsersController@index')->name('users');
+Route::group(['prefix' => 'users/{id}'],function(){
+    Route::get('', 'UsersController@show')->name('user.show');
+    Route::get('favorites','UsersController@favorites')->name('user.favorites');
 
+});
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
     // 動画
@@ -31,5 +34,12 @@ Route::group(['middleware' => 'auth'], function () {
         Route::get('create', 'MoviesController@create')->name('movie.create');
         Route::post('', 'MoviesController@store')->name('movie.store');
         Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+        Route::get('{id}/edit', 'MoviesController@edit')->name('movie.edit');
+        Route::put('{id}', 'MoviesController@update')->name('movie.update');
+    });
+    // いいね
+    Route::group(['prefix' => 'movies/{id}'],function(){
+        Route::post('favorite','FavoriteController@store')->name('favorite');
+        Route::delete('unfavorite','FavoriteController@destroy')->name('unfavorite');
     });
 });
