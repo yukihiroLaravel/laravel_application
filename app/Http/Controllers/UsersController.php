e49f1d6d9be78d7e68f35491df78d6dcd6bf55f9
shunsuke otsubo　 <otsubo.nextstage@gmail.com>
2022-12-05 23:55:52 +0900

    4章　ユーザー一覧、動画投稿機能・削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+        //paginateとは1ページに表示させるのは９つの動画までという記述
+
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,16 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/', 'UsersController@index');
+
+//ログイン後にしか見れないという条件を記述している。midleware =>authでログイン後にしか見れないという条件を追加したことになる
+Route::group(['middleware' => 'auth'],function(){
+
+    //動画　prefixはRouteの最初に書くmoviesというアドレスを省略できるという意味
+    Route::prefix('movies')->group(function()
+    {
+        //＠createで新規登録画面を表示し、@storeで新規登録を実行するのでこの２つはセットで使う
+        Route::get('create','MoviesController@create')->name('movie.create');
+        Route::post('','MoviesController@store')->name('movie.store');
+        Route::delete('{id}','MoviesController@destroy')->name('movie.delete');
+    });
+});
