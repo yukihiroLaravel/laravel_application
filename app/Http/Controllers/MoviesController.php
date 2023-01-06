f01f6d1dff480e811f5fb3f74321e83789d75503
mao-fukushi <zhenyangfushi@gmail.com>
2023-01-06 15:36:53 +0900

    5章 いいね！機能・カラム追加

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -25,6 +25,7 @@ class MoviesController extends Controller
         $movie->youtube_id = $request->youtube_id;
         $movie->title = $request->title;
         $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0;
         $movie->save();
         return back();
     }
@@ -35,6 +36,30 @@ class MoviesController extends Controller
         if (\Auth::id() === $movie->user_id) {
             $movie->delete();
         }
+        return back();
+    }
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $movie = Movie::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movie' => $movie,
+            'movies' => $movies,
+        ];
+        return view('movies.edit', $data);
+    }
+    
+    public function update(MovieRequest $request, $id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->youtube_id = $request->youtube_id;
+        $movie->title = $request->title;
+        $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0;
+        $movie->save();
         return back();
     }
 }
