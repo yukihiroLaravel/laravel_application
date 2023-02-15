789d67726686d89336b005c09164f8357f113c5f
kenta sugihara <kenta03271127@gmail.com>
2023-02-15 21:52:23 +0900

    2章　データベース・マイグレーション・シーダー実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();//ユーザー情報の削除時間をいれる関数　論理削除やさしい削除
         });
     }
 
