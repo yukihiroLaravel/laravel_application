94d9daf133c03969a0a9bcbd1527e98ef49a2b3c
橋本範昭 <nori.hasimo.89@gmail.com>
2023-02-27 01:27:08 +0900

    4章　動画マイグレーションとモデル

---
+++ database/migrations/2023_02_27_001703_create_movies_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('youtube_id');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
