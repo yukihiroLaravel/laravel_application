b867c9e51961e0e4f7af201a8ef1d8bbb0529fa9
tomoakisakizono <bless_aa_0107@icloud.com>
2023-03-06 10:22:08 +0900

    4章 2 動画マイグレーションとモデル

---
+++ app/Movie.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Movie extends Model
+{
+    use SoftDeletes;
+    
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+}
--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -11,7 +11,7 @@
                     <li class="nav-item"><a href="{{ route('logout') }}" class="nav-link">ログアウト</a></li>
                     <li class="nav-item"><a href="" class="nav-link">マイページ</a></li>
                 @else
-                    <li class="nav-item"><a href="" class="nav-link">ログイン</a></li>
+                    <li class="nav-item"><a href="{{ route('login') }}" class="nav-link">ログイン</a></li>
                     <li class="nav-item"><a href="{{ route('signup') }}" class="nav-link">新規ユーザ登録</a></li>
                 @endif
             </ul>
