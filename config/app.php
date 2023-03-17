91ecf7d04a17cd4b7c00bda0a9768ed63a3f1fb2
user <yamadaman.bs@gmail.com>
2023-03-18 01:41:43 +0900

    3章 ログイン機能

--- config/app.php
+++ config/app.php
@@ -66,6 +66,7 @@ return [
     | ahead and set this to a sensible default for you out of the box.
     |
     */
+    
 
     'timezone' => 'Asia/Tokyo',
 
@@ -80,7 +81,7 @@ return [
     |
     */
 
-    'locale' => 'en',
+    'locale' => 'ja',
 
     /*
     |--------------------------------------------------------------------------
