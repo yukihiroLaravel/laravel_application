ac9364ec764510d8a6b79194819da7f1bc5d1128
fukanojyunpei <fukanojyunpei@gmail.com>
2023-03-19 22:01:47 +0900

    4章 ユーザー一覧、動画投稿・削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,13 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\User;
 class UsersController extends Controller
 {
     public function index()
     {
-        $users = User::all();
-        
-        return view('welcome',['user' => $users]);
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 }
