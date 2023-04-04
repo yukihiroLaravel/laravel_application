1b51963a2f9897f65c5c28d21f7ed2f5adba39d2
tomoki amemiya <tomoki423@outlook.com>
2023-04-04 16:27:40 +0900

    3章 ユーザー登録機能

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+@section('content')
+    <div class="text-center">
+        <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>YouTubeまとめ×コミュニケーション</h1>
+    </div>
+    <div class="text-center mt-3">
+        <p class="text-left d-inline-block">新規ユーザ登録すると、<br>あなたのチャンネル作成／動画登録等ができるようになります。</p>
+    </div>
+    <div class="text-center">
+        <h3 class="login_title text-left d-inline-block mt-5">新規ユーザー登録</h3>
+    </div>
+    <div class="row mt-5 mb-5">
+        <div class="col-sm-6 offset-sm-3">
+            <form method="POST" action="{{ route('signup.post') }}">
+                @csrf
+                <div class="form-group">
+                    <label for="name">名前</label>
+                    <input id="name" type="text" class="form-control" name="name"
+                    placeholder="（*必須）" value="{{ old('name') }}">
+                </div>
+                <div class="form-group">
+                    <label for="email">メールアドレス</label>
+                    <input id="email" type="text" class="form-control" name="email" placeholder="（*必須）" value="{{ old('email') }}">
+                </div>
+                <div class="form-group">
+                    <label for="password">パスワード</label>
+                    <input id="password" type="password" class="form-control" name="password" placeholder="（*必須）" value="{{ old('password') }}">
+                </div>
+                <div class="form-group">
+                    <label for="password_confirmation">パスワード確認</label>
+                    <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" placeholder="（*必須）" value="{{ old('password_confirmation') }}">
+                </div>
+                <button type="submit" class="btn btn-primary mt-2">新規登録</button>
+            </form>
+        </div>
+    </div>
+@endsection
+
+<!-- 各項目にplaceholder="（*必須）"追加 -->
