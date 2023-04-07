34b770325c1797b352177b7d12a170fb9b4678e9
masahiro-mera <romasahiro5612@gmail.com>
2023-04-08 00:29:34 +0900

    6章　RESTful API

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -6,22 +6,44 @@
         @endif
             <div class="col-lg-4 mb-5">
                 <div class="movie text-left d-inline-block">
-                    <div>
-                        @if ($movie)
-                            <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}" frameborder="0"></iframe>
+                @php
+                        $countFavoriteUsers = $movie->favoriteUsers()->count();
+                        $videoTitle="※動画が未登録です";
+                        if ($movie) {
+                            $keyName = config('app.YouTubeDataApiKey');
+                            $apiUrl = "https://www.googleapis.com/youtube/v3/videos?id={$movie->youtube_id}&key={$keyName}&part=snippet";
+                            $jsonData = file_get_contents($apiUrl);
+                            if ($jsonData) {
+                                $decodedData = json_decode($jsonData, true);
+                                if ($decodedData['pageInfo']['totalResults'] !== 0){
+                                    $videoTitle = $decodedData['items']['0']['snippet']['title'];
+                                }
+                            } else {
+                                $videoTitle="※一時的な情報制限中です";
+                            }
+                        }
+                        @endphp
+                    <div class="text-right mb-2">いいね！
+                        <span class="badge badge-pill badge-success">{{ $countFavoriteUsers }}</span>
+                    </div>
+                        @if($movies)                      
+                         <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}" frameborder="0"></iframe>
                         @else
                             <iframe width="290" height="163.125" src="https://www.youtube.com/embed/" frameborder="0"></iframe>
                         @endif
                     </div>
                     <p>
-                        @if (isset($movie->title))
+                    @if (isset($movie->title))
                             {{ $movie->title }}
+                        @else
+                            {{ $videoTitle }}
                         @endif
                     </p>
                     @if (isset($movie->comment))
                             {{ $movie->comment }}
                         @endif
                     </p>
+                    @include('favorite.favorite_button', ['movie' => $movie])
                     @if (Auth::id() === $movie->user_id)
                         <form method="POST" action="{{ route('movie.delete', $movie->id) }}">
                             @csrf
--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -2,7 +2,26 @@
 <div class="movies row mt-5 text-center">
     @foreach ($users as $user)
         @php
+        $movies = $user->movies()->get();
+            $totalFavorites = 0;
+            foreach ($movies as $movie){
+                $totalFavorites += $movie->favoriteUsers()->count();
+            }
             $movie = $user->movies->last();
+            $videoTitle="※動画が未登録です";
+            if ($movie) {
+                $keyName = config('app.YouTubeDataApiKey');
+                $apiUrl = "https://www.googleapis.com/youtube/v3/videos?id={$movie->youtube_id}&key={$keyName}&part=snippet";
+                $jsonData = file_get_contents($apiUrl);
+                if ($jsonData) {
+                    $decodedData = json_decode($jsonData, true);
+                    if ($decodedData['pageInfo']['totalResults'] !== 0){
+                        $videoTitle = $decodedData['items']['0']['snippet']['title'];
+                    }
+                } else {
+                    $videoTitle="※一時的な情報制限中です";
+                }
+            }
         @endphp
         @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
             </div>
@@ -10,6 +29,9 @@
         @endif
             <div class="col-lg-4 mb-5">
                 <div class="movie text-left d-inline-block">
+                    <div class="text-right">
+                        <span class="badge badge-pill badge-success">{{ $totalFavorites }} いいね!</span>
+                    </div>
                 <a href="{{ route('user.show', $user->id) }}">＠{{ $user->name }}</a>
                     <div>
                         @if ($movie)
@@ -21,6 +43,8 @@
                     <p>
                         @if (isset($movie->title))
                             {{ $movie->title }}
+                        @else
+                            {{ $videoTitle }}
                         @endif
                     </p>
                 </div>
