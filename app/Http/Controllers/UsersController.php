2d7b3970c6acc23d84ebb485991e147658847157
yuki ninomiya <kyuu.195@gmail.com>
2023-04-08 13:53:28 +0900

    ５章　いいね！機能・カラム追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,6 +25,20 @@ class UsersController extends Controller
             'movies' => $movies,
         ];
         $data += $this->userCounts($user);
+
         return view('users.show',$data);
+    }
+
+    public function favorites($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->favorites()->paginate(9);
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        
+        return view('users.show', $data);
     }
 }
