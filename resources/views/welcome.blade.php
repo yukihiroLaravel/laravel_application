8d7d67106f4b97e4720df51f24989648951b5646
yuki ohashi <yukiohashi1119@gmail.com>
2023-04-10 22:41:39 +0900

    4章　ユーザ一覧・動画投稿・削除機能

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,9 +2,9 @@
 @section('content')
     <div class="center jumbotron bg-dark">
         <div class="text-center text-white mt-2 pt-1">
-            <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>YouTubeまとめ</h1>
-            <h1>× コミュニケーション</h1>
+            <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>YouTubeまとめ<br>× コミュニケーション</h1>
         </div>
     </div>
     <h5 class="description text-center">みんなの"オススメ"動画を自由にシェアしよう</h5>
-@endsection
+    @include('users.users', ['users' => $users])
+@endsectionß
