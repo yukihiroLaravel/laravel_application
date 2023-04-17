84ad2f1671615f79a21b78ff10ea441addabeef7
toshiki kawauchi <toshiki242zwo@gmail.com>
2023-04-17 23:45:53 +0900

    5章 いいね！機能・カラム追加

--- app/Movie.php
+++ app/Movie.php
@@ -11,7 +11,12 @@ class Movie extends Model
 
     public function user()
     {
-        return $this->belongsTo(User::class);
+        return $this->belongsTo(User::class);  
+    }
+
+    public function favoriteUsers()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'movie_id', 'user_id')->withTimestamps();
     }
 }
 
