3fcf7387e549089dd5b08b0196dafacd8baeb8b9
toshiki kawauchi <toshiki242zwo@gmail.com>
2023-04-30 05:49:26 +0900

    2章データベース・マイグレーション・シーダー実装

--- app/User.php
+++ app/User.php
@@ -5,10 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes;
 
     /**
      * The attributes that are mass assignable.
@@ -36,4 +38,41 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function movies()
+    {
+        return $this->hasMany(Movie::class);
+    }
+
+    public function favorites()
+    {
+        return $this->belongsToMany(Movie::class, 'favorites', 'user_id', 'movie_id')->withTimestamps();
+    }
+
+    public function favorite($movieId)
+    {
+        $exist = $this->isFavorite($movieId);
+        if ($exist) {
+            return false;
+        } else {
+            $this->favorites()->attach($movieId);
+            return true;
+        }
+    }
+
+    public function unfavorite($movieId)
+    {
+        $exist = $this->isFavorite($movieId);
+        if ($exist) {
+            $this->favorites()->detach($movieId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    public function isFavorite($movieId)
+    {
+        return $this->favorites()->where('movie_id', $movieId)->exists();
+    }
 }
