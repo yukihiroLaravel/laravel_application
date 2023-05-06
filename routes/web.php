5b6cdc80bba918932112760ccfa08292ea799214
u0222 <ootayy411@gmail.com>
2023-05-07 06:43:56 +0900

    3章 ログイン機能

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,9 @@
 Route::get('/', 'UsersController@index'); // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
