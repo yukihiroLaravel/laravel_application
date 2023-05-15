331296c8dbed360f0d91f723acb69c5fb08b60c2
seiichi ozawa <sasutinaburu@yahoo.co.jp>
2023-05-15 18:38:41 +0900

    4章　動画マイグレーションとモデル

--- app/User.php
+++ app/User.php
@@ -38,4 +38,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+    {
+        return $this->hasMany(Movie::class);
+    }
+
 }
+
+
+
