76772f5a6041ff2faf9d3639106c2be3ea1481f8
seiichi ozawa <sasutinaburu@yahoo.co.jp>
2023-05-22 11:20:49 +0900

    4章 動画登録・ユーザ詳細

--- app/User.php
+++ app/User.php
@@ -38,10 +38,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+     public function movies()
     {
         return $this->hasMany(Movie::class);
     }
-
 }
 
 
