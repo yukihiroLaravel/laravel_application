268a09e44ba259dc2bbf45f53bd2df9ac2a88e27
Hikaru Matsunaga <nyanderful9@gmail.com>
2023-05-29 01:33:55 +0900

    5章 いいね！機能・カラム追加

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -14,8 +14,10 @@ class Controller extends BaseController
     public function userCounts($user)
     {
         $countMovies = $user->movies()->count();
+        $countFavorites = $user->favorites()->count();
         return [
             'countMovies' => $countMovies,
+            'countFavorites' => $countFavorites,
         ];
     }
 }
