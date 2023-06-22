ea355f4f2f0d9901a79c519ba2ea496a09fe7b82
shinsuke kurokawa <x14pro1999@gmail.com>
2023-06-22 20:41:36 +0900

    3章 トップページの表示

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,12 @@
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-dark justify-content-center">
+        <span class="navbar-brand">@GUt Familie,All rights reserved.</span>
+    </nav>
+</footer>    
+
+
+
+
+
+
+
