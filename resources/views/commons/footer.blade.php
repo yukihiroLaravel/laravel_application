d3bcbc6e007e650a0edac0b868e9923bf4bae4e6
harunobu murai <mu19910624rai@gmail.com>
2023-07-06 07:04:21 +0900

    3章　トップページの表示

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,5 @@
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-dark justyfy-content-center">
+        <span class="navbar-brand">©Gut Familie, All rights reserved.</span>
+    </nav>
+</footer>
