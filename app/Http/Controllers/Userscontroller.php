34812c63b4127ded7d3c8605dd6b0a84d21648a6
harunobu murai <mu19910624rai@gmail.com>
2023-07-14 12:04:22 +0900

    ユーザ一覧、動画投稿・削除

---
+++ app/Http/Controllers/Userscontroller.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User; // 追記
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome', [
+            'users' => $users,
+        ]);
+    }
+}
