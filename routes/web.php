9960fd6d8bd4f830397acf3f814db9372125df1a
shunsuke otsubo <otsubo.nextstage@gmail.com>
2023-07-26 07:36:48 +0900

    3章　ログイン機能

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,10 @@ Route::get('signup','Auth\RegisterController@showRegistrationForm')->name('signu
 //ユーザー登録の実行
 Route::post('signup','Auth\RegisterController@register')->name('signup.post');
 
+//ログイン機能
+Route::get('login','Auth\LoginController@showLoginForm')->name('login');
+Route::post('login','Auth\LoginController@login')->name('login.post');
+Route::get('logout','Auth\LoginController@logout')->name('logout');
 
 Route::get('/','UsersController@index');
 
