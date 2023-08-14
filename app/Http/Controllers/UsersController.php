a2318b9db1c26360352181ffac92c63ec9d5b6bf
yasuyuki nakamura <yasu98@docomo.ne.jp>
2023-08-14 13:44:49 +0900

    4章動画登録・ユーザ登録

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -16,4 +16,18 @@ class UsersController extends Controller
             'users' => $users,
         ]);
     }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
+    }
+
+
 }
