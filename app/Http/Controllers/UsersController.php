23922c3d6b4f6d60a24bae68e3661ee86e00e9f1
yasuyuki nakamura <yasu98@docomo.ne.jp>
2023-08-14 11:26:59 +0900

    ４章ユーザー一覧、動画投稿・削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Movie;
+use App\Http\Requests\MovieRequest;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 }
