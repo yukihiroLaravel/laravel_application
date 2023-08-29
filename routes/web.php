76b582e88f7fd22be10b279b118c35d45ff2792f
ma34vn <makino_vn@yahoo.co.jp>
2023-08-29 21:19:25 +0900

    3章ログイン機能

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', 'UsersController@index');
 // ユーザ新規登録
 Route::get('singup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('singup', 'Auth\RegisterController@register')->name('signup.post');
+
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginControloler@logout')->name('logout');
+
