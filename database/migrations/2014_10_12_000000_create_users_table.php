1f4d830e1bc704bebff8f46e0444576de2e36c64
jyunya kawabata <fc3c1213@gamil.com>
2023-09-07 19:30:26 +0900

    2章データベース.マイグレーション.シーダー実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+        
 class CreateUsersTable extends Migration
 {
     /**
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'test11',
+            'email' => 'test11@test.com',
+            'password' => bcrypt('test11')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test12',
+            'email' => 'test12@test.com',
+            'password' => bcrypt('test12')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test13',
+            'email' => 'test13@sample.com',
+            'password' => bcrypt('test13')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test14',
+            'email' => 'test14@test.com',
+            'password' => bcrypt('test14')
+        ]);
+
+    }
+}
