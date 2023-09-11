1b70fc6c31b9cd8a3b97d3af65a7bc6924c3b8ad
kiichi naoi <7nk.kiichi.orange@gmail.com>
2023-09-11 22:16:51 +0900

    4章 動画登録・ユーザ詳細

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,8 +6,28 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use App\User;
 
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    public function userCounts($user)
+    {
+        $countMovies = $user->movies()->count();
+        return [
+            'countMovies' => $countMovies,
+        ];
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data = [
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
+    }
 }
