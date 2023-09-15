df90ad7ed544f4dcab8667c41dcc1ad8bc75a5ce
kiichi naoi <7nk.kiichi.orange@gmail.com>
2023-09-15 22:06:22 +0900

    2章 データベース・マイグレーション・シーダー実装

--- config/app.php
+++ config/app.php
@@ -67,8 +67,7 @@ return [
     |
     */
 
-    'timezone' => 'UTC',
-
+    'timezone' => 'Asia/Tokyo',
     /*
     |--------------------------------------------------------------------------
     | Application Locale Configuration
