48d72e4935e90d500be24e6a06a8778d6e7b5e52
hatschi2023 <hatschi.sachi@gmail.com>
2023-09-23 15:37:16 +0900

    2章 データベース・マイグレーション・シーダー実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,6 +21,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes(); //  追加 2023.09.23　
         });
     }
 
