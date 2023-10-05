5ea9d693e44d51a799af306d3dcfc548046a6678
hatschi2023 <hatschi.sachi@gmail.com>
2023-10-05 21:12:50 +0900

    4章　動画登録・ユーザ詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,15 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
+use App\User; 
 
 class UsersController extends Controller
 {
-    public function index() 
+    public function index()
     {
         $users = User::orderBy('id','desc')->paginate(9);
         return view('welcome', [
             'users' => $users,
         ]);
+        dd($users);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -20,16 +20,19 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/', 'UsersController@index'); 
+// ユーザ
+Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
+    // 動画
     Route::prefix('movies')->group(function () {
         Route::get('create', 'MoviesController@create')->name('movie.create');
         Route::post('', 'MoviesController@store')->name('movie.store');
         Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
     });
 });
-
-
