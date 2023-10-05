950bac3e188af131b6829859bd32e587124e1e25
hatschi2023 <hatschi.sachi@gmail.com>
2023-10-05 15:27:21 +0900

    4章　ユーザー一覧、動画投稿・削除機能

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,16 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-Route::get('/', 'UsersController@index');  //追加　2023.09.23
+Route::get('/', 'UsersController@index'); 
+
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+    Route::prefix('movies')->group(function () {
+        Route::get('create', 'MoviesController@create')->name('movie.create');
+        Route::post('', 'MoviesController@store')->name('movie.store');
+        Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+    });
+});
+
 
