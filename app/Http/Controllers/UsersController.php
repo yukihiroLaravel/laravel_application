c65c0fd646e75cd216f7e06ea892d2413212e822
hatschi2023 <hatschi.sachi@gmail.com>
2023-11-19 23:19:58 +0900

    4章 動画登録・ユーザ詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,6 +8,21 @@ class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome',[
+            'users'=>$users,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id','desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show', $data);
     }
 }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,7 @@
     <title>YouTubeまとめ×コミュニケーション</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
 </head>
 <body>
     @include('commons.header')
@@ -19,3 +20,4 @@
     <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
 </body>
 </html>
+
