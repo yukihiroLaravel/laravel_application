9eed424a59d07d8a0e82d69aad0f255afd5dfb66
daisuke ide <win10yamaha@outlook.jp>
2023-11-22 19:59:28 +0900

    ４章　ユーザー一覧、動画投稿・削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,20 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
+use App\Movie;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view ('welcome');
+        $user = User::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show',$data);
     }
 }
