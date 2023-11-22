6d71e1705f9832a523fa887292e41b28558bd1e4
daisuke ide <win10yamaha@outlook.jp>
2023-11-22 20:35:32 +0900

    ４章　動画登録・ユーザ詳細

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -10,6 +10,14 @@ class UsersController extends Controller
 {
     public function index()
     {
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome', [
+            'users' => $users,
+        ]);
+    }
+
+    public function show($id)
+    {
         $user = User::findOrFail($id);
         $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
         $data=[
