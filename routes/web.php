c2f91b6d4d5b9127b85eeea34621c91e322d01d1
abeai0 <115936489+abeai0@users.noreply.github.com>
2023-11-28 13:56:35 +0900

    4章　ユーザー一覧、動画投稿・削除機能

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 // ユーザー新規登録
+
+use Illuminate\Routing\RouteGroup;
+
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
@@ -20,6 +23,16 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-
-
+// トップページ表示
 Route::get('/', 'UsersController@index');
+
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+  // 動画
+  Route::prefix('movies')->group(function () {
+    Route::get('create', 'MoviesController@create')->name('movie.create');
+    Route::post('', 'MoviesController@store')->name('movie.store');
+    Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+  });
+});
