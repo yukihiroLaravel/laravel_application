0dd9f5c8bbbea8d470d7983ad27f67db5afe85c1
abeai0 <115936489+abeai0@users.noreply.github.com>
2023-11-28 16:53:57 +0900

    4章　動画登録・ユーザー詳細

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,10 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // トップページ表示
 Route::get('/', 'UsersController@index');
+// ユーザー詳細ページ
+Route::prefix('users')->group(function () {
+  Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 
 // ログイン後
