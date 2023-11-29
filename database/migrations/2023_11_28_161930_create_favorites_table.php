37f9a1e62df20cd69cce32f7eea3dbd2a5934be3
daisuke ide <win10yamaha@outlook.jp>
2023-11-29 17:06:26 +0900

    ５章　いいね！機能・カラム追加

--- database/migrations/2023_11_28_161930_create_favorites_table.php
+++ database/migrations/2023_11_28_161930_create_favorites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ModelMovie extends Migration
+class CreateFavoritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,15 +13,15 @@ class ModelMovie extends Migration
      */
     public function up()
     {
-        Schema::create('movies', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('title')->nullable();
             $table->bigInteger('user_id')->unsigned()->index();
-            $table->string('youtube_id');
+            $table->bigInteger('movie_id')->unsigned()->index();
             $table->timestamps();
-            $table->softDeletes();
             // 外部キー制約
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->foreign('movie_id')->references('id')->on('movies')->onDelete('cascade');
+            $table->unique(['user_id','movie_id']);
         });
     }
 
@@ -32,6 +32,6 @@ class ModelMovie extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('movies');
+        Schema::dropIfExists('favorites');
     }
 }
