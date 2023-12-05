949e51df101c252358b7cf3947f30011163d17ee
yuya kondo <3939sereson@gmail.com>
2023-12-05 15:45:57 +0900

    6章 RESTful API

--- config/app.php
+++ config/app.php
@@ -225,7 +225,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-
     ],
+
+    //API_KEY
+    'YoutubeDataApiKey' => env('API_KEY'),
 
 ];
