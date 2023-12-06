4d78ba06e08a0ab0b44cddc7674dde7f58cf9af8
abeai0 <115936489+abeai0@users.noreply.github.com>
2023-12-06 14:51:28 +0900

    5章　いいね！機能カラム追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -25,4 +25,17 @@ class UsersController extends Controller
         $data += $this->userCounts($user);
         return view('users.show', $data);
     }
+
+    public function favorites($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->favorites()->paginate(9);
+        $data = [
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+
+        return view('users.show', $data);
+    }
 }
--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,36 +1,46 @@
 <h2 class="mt-5 mb-5">チャンネル一覧</h2>
 <div class="movies row mt-5 text-center">
-  @foreach ($users as $user)
-  @php
-  $movie = $user->movies->last();
-  @endphp
-  @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
+    @foreach ($users as $user)
+        @php
+            $movies = $user->movies()->get();
+            $totalFavorites = 0;
+            foreach ($movies as $movie) {
+                $totalFavorites += $movie->favoriteUsers()->count();
+            }
+            $movie = $user->movies->last();
+        @endphp
+        @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
 </div>
 <div class="row text-center mt-3">
-  @endif
-  <div class="col-lg-4 mb-5">
-    <div class="movie text-left d-inline-block">
+    @endif
+    <div class="col-lg-4 mb-5">
+        <div class="movie text-left d-inline-block">
+            <div class="text-right">
+                <span class="badge badge-pill badge-success">
+                    {{ $totalFavorites }} いいね！
+                </span>
+            </div>
+            <a href="{{ route('user.show', $user->id) }}">
+                ＠{{ $user->name }}
+            </a>
 
-      <a href="{{ route('user.show', $user->id) }}">
-        ＠{{ $user->name }}
-      </a>
-
-      <div>
-        @if ($movie)
-        <iframe width="290" height="163.125"
-          src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}"
-          frameborder="0"></iframe>
-        @else
-        <iframe width="290" height="163.125" src="https://www.youtube.com/embed/" frameborder="0"></iframe>
-        @endif
-      </div>
-      <p>
-        @if (isset($movie->title))
-        {{ $movie->title }}
-        @endif
-      </p>
+            <div>
+                @if ($movie)
+                    <iframe width="290" height="163.125"
+                        src="{{ 'https://www.youtube.com/embed/' . $movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}"
+                        frameborder="0"></iframe>
+                @else
+                    <iframe width="290" height="163.125" src="https://www.youtube.com/embed/"
+                        frameborder="0"></iframe>
+                @endif
+            </div>
+            <p>
+                @if (isset($movie->title))
+                    {{ $movie->title }}
+                @endif
+            </p>
+        </div>
     </div>
-  </div>
-  @endforeach
+    @endforeach
 </div>
 {{ $users->links('pagination::bootstrap-4') }}
