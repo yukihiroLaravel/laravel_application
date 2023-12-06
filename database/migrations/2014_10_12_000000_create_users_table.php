f3440d25612a3ca8e1b58803299d3b7b80850393
kiyoharu <amaminikimasita@gmail.com>
2023-12-07 01:09:50 +0900

    ２章データベース・マイグレーション・シーダー実装７

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
             $table->softDeletes();
-            
+
         });
     }
 
