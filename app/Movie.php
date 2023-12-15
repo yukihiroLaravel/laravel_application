fc71c820030332db2c1109d9d5f40e09c480929a
kenta oohashi <kerutomusic@gmail.com>
2023-12-16 02:00:35 +0900

    4章 ユーザー一覧、動画投稿・削除

--- app/Movie.php
+++ app/Movie.php
@@ -3,7 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Databese\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Movie extends Model
 {
