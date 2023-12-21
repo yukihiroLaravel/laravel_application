1a40b7ab3349995712b23839a2aa9d41811f220b
kiyoharu <yukihiro@example.com>
2023-12-21 21:51:45 +0900

    第4章動画登録・ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,13 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-
-
-Route::get('/', 'UsersController@index');
-
 // ユーザ
-Route::get('/', 'UsersController@index')->name('users');
-Route::group(['prefix' => 'users/{id}'],function(){
-    Route::get('', 'UsersController@show')->name('user.show');
-    Route::get('favorites','UsersController@favorites')->name('user.favorites');
+Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
 });
+
+
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
     // 動画
