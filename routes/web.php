448eddf0d25dd600f8451770ce4ce170fa4d9b14
saitoh <mintorimoto@gmail.com>
2024-01-04 18:49:56 +0900

    4章 ユーザ一覧動画投稿・削除

--- routes/web.php
+++ routes/web.php
@@ -12,8 +12,7 @@
 */
 
 // ユーザ新規登録
-//Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
-//Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
@@ -22,6 +21,15 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+    // 動画
+    Route::prefix('movies')->group(function () {
+        Route::get('create', 'MoviesController@create')->name('movie.create');
+        Route::post('', 'MoviesController@store')->name('movie.store');
+        Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+    });
+});
 
 
 Route::get('/', 'UsersController@index');
