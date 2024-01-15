f5f06de634a54310d019fe8c599ff33f72e907c5
saitoh <mintorimoto@gmail.com>
2024-01-15 16:04:49 +0900

    2章 いいね！機能・カラム追加

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -26,10 +26,10 @@ class MoviesController extends Controller
         $movie->youtube_id = $request->youtube_id;
         $movie->title = $request->title;
         $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0; //追記
         $movie->save();
         return back();
     }
-
     public function destroy($id)
     {
         $movie = Movie::findOrFail($id);
@@ -38,4 +38,36 @@ class MoviesController extends Controller
         }
         return back();
     }
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $movie = Movie::findOrFail($id);
+        $movies = $user->movies()->orderBy('id','desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movie' => $movie,
+            'movies' => $movies,
+        ];
+        
+        return view('movies.edit',$data);
+    }
+    public function update(MovieRequest $request, $id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->youtube_id = $request->youtube_id;
+        $movie->title = $request->title;
+        $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0; //追記
+        $movie->save();
+        return back();
+    }
+
+    
+    
+
+
+
+
+
 }
--- app/Movie.php
+++ app/Movie.php
@@ -12,4 +12,12 @@ class Movie extends Model
     {
         return $this->belongsTo(User::class);
     }
+   
+    
+
+    public function favoriteUsers()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'movie_id', 'user_id')->withTimestamps();
+    }
+
 }
