ebb479943b24eaa6b6b60904831f875939ea7af4
deuro-ki <hero14yki1986@gmail.com>
2024-01-18 18:51:06 +0900

    5章 いいね！機能・カラム追加

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -20,12 +20,37 @@ class MoviesController extends Controller
         return view('movies.create', $data);
     }
 
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $movie = Movie::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movie' => $movie,
+            'movies' => $movies,
+        ];
+        return view('movies.edit', $data);
+    }
+    
+    public function update(MovieRequest $request, $id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->youtube_id = $request->youtube_id;
+        $movie->title = $request->title;
+        $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0;
+        $movie->save();
+        return back();
+    }
+
     public function store(MovieRequest $request)
     {
         $movie = new Movie;
         $movie->youtube_id = $request->youtube_id;
         $movie->title = $request->title;
         $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0;
         $movie->save();
         return back();
     }
