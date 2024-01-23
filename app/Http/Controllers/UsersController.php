c05903a7255bb334969611d534cc6677b5e35dfd
masashitamayama <okita-souji.7-7-3@outlook.jp>
2024-01-23 14:54:00 +0900

    4章 動画マイグレーションとモデル

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -6,6 +6,10 @@ use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
+    // public function index()
+    // {
+    //     return view('welcome');
+    // }
     public function index()
     {
         return view('welcome');
