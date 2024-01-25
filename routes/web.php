6182f1fd84f7034468323aba640d37e9b40bfdcd
deuro-ki <hero14yki1986@gmail.com>
2024-01-26 03:55:34 +0900

    4章 動画登録・ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,12 @@
 |
 */
 
+// ユーザ
 Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+  Route::get('{id}', 'UsersController@show')->name('user.show');
+});
+
 
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
@@ -21,3 +26,14 @@ Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+  // 動画
+  Route::prefix('movies')->group(function () {
+      Route::get('create', 'MoviesController@create')->name('movie.create');
+      Route::post('', 'MoviesController@store')->name('movie.store');
+      Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+  });
+});
+
