f07d6e9fe99a28fbe1fd779fb5d9a74c284681fb
saitoh <mintorimoto@gmail.com>
2024-02-02 21:30:24 +0900

    2章 データベース・マイグレーション・シーダー実装

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        
+        DB::table('users')->insert([
+            'name' => 'test5',
+            'email' => 'test5@test.com',
+            'password' => bcrypt('test5')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test6',
+            'email' => 'test6@test.com',
+            'password' => bcrypt('test6')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test7',
+            'email' => 'test7@test.com',
+            'password' => bcrypt('test7')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test8',
+            'email' => 'test8@test.com',
+            'password' => bcrypt('test8')
+        ]);
+    }
+}
+
