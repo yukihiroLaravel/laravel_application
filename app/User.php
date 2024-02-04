e489ce0da4b1311599f35670af788ed5d18b4778
saitoh <mintorimoto@gmail.com>
2024-02-04 16:49:50 +0900

    4章動画マイグレーションとモデル

--- app/User.php
+++ app/User.php
@@ -39,4 +39,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function movies()
+    {
+        return $this->hasMany(Movie::class);
+    }
+    
+
 }
