d0105c5a13b1be253bb89264365e0bb4ba99892e
yuta <36120057+tokechan@users.noreply.github.com>
2024-02-10 18:47:20 +0900

    2章データベース、マイグレーション、シーダー実装２

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,24 +12,44 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            'name' => 'test1',
-            'email' => 'test1@test.com',
-            'password' => bcrypt('test1')
+            'name' => 'test10',
+            'email' => 'test10@test.com',
+            'password' => bcrypt('test10')
         ]);
         DB::table('users')->insert([
-            'name' => 'test2',
-            'email' => 'test2@test.com',
-            'password' => bcrypt('test2')
+            'name' => 'test20',
+            'email' => 'test20@test.com',
+            'password' => bcrypt('test20')
         ]);
         DB::table('users')->insert([
-            'name' => 'test3',
-            'email' => 'test3@test.com',
-            'password' => bcrypt('test3')
+            'name' => 'test30',
+            'email' => 'test30@test.com',
+            'password' => bcrypt('test30')
         ]);
         DB::table('users')->insert([
-            'name' => 'test4',
-            'email' => 'test4@test.com',
-            'password' => bcrypt('test4')
+            'name' => 'test40',
+            'email' => 'test40@test.com',
+            'password' => bcrypt('test40')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test5',
+            'email' => 'test5@test.com',
+            'password' => bcrypt('test5')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test6',
+            'email' => 'test6@test.com',
+            'password' => bcrypt('test6')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test7',
+            'email' => 'test7@test.com',
+            'password' => bcrypt('test7')
+        ]);
+        DB::table('users')->insert([
+            'name' => 'test8',
+            'email' => 'test8@test.com',
+            'password' => bcrypt('test8')
         ]);
     }
 }
