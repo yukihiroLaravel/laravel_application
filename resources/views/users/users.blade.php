f58843cd34d1362a7f8896b49d14cf9c6923d5e3
manabu tawaratsumida <andybellphp@gmail.com>
2024-03-06 14:17:11 +0900

    第5章　いいね！機能・カラム追加

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -2,7 +2,12 @@
 <div class="movies row mt-5 text-center">
     @foreach ($users as $user)
         @php
-            $movie = $user->movies->last();
+            $movies = $user->movies()->get();
+            $totalFavorites = 0;
+            foreach ($movies as $movies){
+                $totalFavorites += $movies->favoriteUsers()->count();
+            }
+                $movie = $user->movies->last();
         @endphp
         @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
             </div>
@@ -10,7 +15,10 @@
         @endif
             <div class="col-lg-4 mb-5">
                 <div class="movie text-left d-inline-block">
-                <a href="{{ route('user.show', $user->id) }}">＠{{ $user->name }}</a>
+                    <div class="text-right">
+                        <span class="badge badge-pill badge-success">{{ $totalFavorites }} いいね!</span>
+                    </div>
+                    <a href="{{ route('user.show', $user->id) }}">＠{{ $user->name }}</a>
                     <div>
                         @if ($movie)
                             <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}" frameborder="0"></iframe>
--- routes/web.php
+++ routes/web.php
@@ -11,30 +11,33 @@
 |
 */
 
-//ユーザ登録
+//ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
-
-//ログイン登録
+//ログイン
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
-
-Route::get('/', 'UsersController@index');
-
-//ログイン後
-Route::group(['middleware' => 'auth'], function () {
-    //動画
-    Route::prefix('movies')->group(function () {
-        Route::get('create', 'MoviesController@create')->name('movie.create');
-        Route::post('', 'MoviesController@store')->name('movie.store');
-        Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
-    });
-});
 
 //ユーザ
 Route::get('/', 'UsersController@index');
 Route::group(['prefix' => 'users/{id}'],function() {
     Route::get('', 'UsersController@show')->name('user.show');
+    Route::get('favorites', 'UsersController@favorites')->name('user.favorites'); 
+    });
+//ログイン後
+Route::group(['middleware' => 'auth'], function () {
+//動画
+Route::prefix('movies')->group(function () {
+    Route::get('create', 'MoviesController@create')->name('movie.create');
+    Route::post('', 'MoviesController@store')->name('movie.store');
+    Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+    Route::get('{id}/edit', 'MoviesController@edit')->name('movie.edit');
+    Route::put('{id}', 'MoviesController@update')->name('movie.update');
+ });
+//いいね
+Route::group(['prefix' => 'movies/{id}'],function(){
+    Route::post('favorite','FavoriteController@store')->name('favorite');
+    Route::delete('unfavorite','FavoriteController@destroy')->name('unfavorite');
+    });
 });
-
