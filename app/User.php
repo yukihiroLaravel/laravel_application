5ec8417094b8900d2939d0bbdd4592681aba7c32
X <X@X.com>
2024-03-12 19:32:51 +0900

    2章データベース・マイグレーション・シーダー実装

--- app/User.php
+++ app/User.php
@@ -5,11 +5,13 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\softDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
-
+    use softDeletes;
+    
     /**
      * The attributes that are mass assignable.
      *
