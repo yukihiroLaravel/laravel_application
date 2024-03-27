97165a6ccba4ccb3e38dd8b63f408a5bf3a2a830
yuta <36120057+tokechan@users.noreply.github.com>
2024-03-27 18:51:46 +0900

    ３章　トップページの表示

---
+++ resources/views/commons/footer.blade.php
@@ -0,0 +1,6 @@
+
+<footer class="mt-5">
+    <nav class="navbar navbar-dark bg-dark justify-content-center">
+        <span class="navbar-brand">©Gut Familie, All rights reserved.</span>
+    </nav>
+</footer>
---
+++ resources/views/commons/header.blade.php
@@ -0,0 +1,15 @@
+<header class="mb-5">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">YouTubeSummary<br>&ensp;×<br>Communication</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-bar">
+            <ul class="navbar-nav mr-auto"></ul>
+            <ul class="navbar-nav">
+                <li class="nav-item"><a href="" class="nav-link">Legister</a></li>
+                <li class="nav-item"><a href="" class="nav-link">Log in</a></li>
+            </ul>
+        </div>
+    </nav>
+</header>
