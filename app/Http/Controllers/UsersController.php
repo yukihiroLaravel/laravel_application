47a43caec1bd74271e83def1404700d605095b3b
mtakk <forsyuka321@gmail.com>
2024-04-05 17:35:57 +0900

    4章　ユーザ一覧、動画投稿・削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+        // view上に変数を持っていく時は配列を使う
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 }
