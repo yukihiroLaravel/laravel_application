7aa42709532f6180dfae588865eaab6245e6c696
mtakk <forsyuka321@gmail.com>
2024-04-07 14:34:29 +0900

    5章　いいね！機能・カラム追加

--- app/Movie.php
+++ app/Movie.php
@@ -13,4 +13,9 @@ class Movie extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    public function favoriteUsers()
+    {
+        return $this->belongsToMany(User::class, 'favorites', 'movie_id', 'user_id')->withTimestamps();
+    }
 }
