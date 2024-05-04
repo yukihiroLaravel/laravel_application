f38029a51b9244a052f7769275335cdb0857aa74
masaaki tsuji <masaaki@example.com>
2024-05-04 22:43:25 +0900

    動画登録・ユーザー詳細

--- routes/web.php
+++ routes/web.php
@@ -35,6 +35,9 @@ Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 // ユーザ
 Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 
 
@@ -48,7 +51,7 @@ Route::group(['middleware' => 'auth'], function () {
     // 動画
     Route::prefix('movies')->group(function () {
         Route::get('create', 'MoviesController@create')->name('movie.create');
-        Route::post('', 'MoviesController@store')->name('movie.store');
+        Route::post('/', 'MoviesController@store')->name('movie.store');
         Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
     });
 });
