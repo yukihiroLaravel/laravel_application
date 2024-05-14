e58bdb021449e6d1059ba5da54ece817ce0e091d
eri shimizu <poire12@outlook.jp>
2024-05-14 16:07:54 +0900

    3章　ユーザー登録機能

--- routes/web.php
+++ routes/web.php
@@ -11,4 +11,8 @@
 |
 */
 
+//ユーザー登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
 Route::get('/', 'UserController@index');
