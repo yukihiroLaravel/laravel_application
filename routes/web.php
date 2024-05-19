433a4020897eb76a9665ffc1457caf565c229a37
yasushi sato <xyz.20131984@gmail.com>
2024-05-19 17:00:48 +0900

    3章 トップページの表示

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+//  前半：条件＝こうしたら。。。
+// 　　　　⇒送信形式：見たい（GET）or 伝えたい（POST）
+// 　　　　⇒URL：住所
+// 　　　　⇒Route:get('/',
+
+//  後半：実行内容＝こうなる！
+// 　　　　⇒ファイル名@アクション（関数）：どのファイルのどんな結果？
+// 　　　　　例）合えるのか、付き合えるのか、フラれるのか？
+// 　　　　⇒'UsersController@index');
+Route::get('/','UsersController@index');
