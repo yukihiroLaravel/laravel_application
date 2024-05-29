d114c293a62398ee40a4d9850e7a58462071fc1d
eri shimizu <poire12@outlook.jp>
2024-05-29 17:47:45 +0900

    4章　ユーザー一覧、動画投稿・削除

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\User;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        $users = User::orderBy('id','desc')->paginate(9);
+        
+        return view('welcome', [
+            'users' => $users,
+        ]);
+    }
+}
