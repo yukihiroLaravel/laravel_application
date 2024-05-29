b1c4fe9d743d1a21f4e60df5fdd69ab93e46c2a7
abeai0 <115936489+abeai0@users.noreply.github.com>
2024-05-29 13:57:54 +0900

    グーグルログイン変更

--- config/services.php
+++ config/services.php
@@ -31,9 +31,9 @@ return [
     ],
 
     'google' => [
-        'client_id' => env('GOOGLE_KEY'),
-        'client_secret' => env('GOOGLE_SECRET'),
-        'redirect' => env('GOOGLE_REDIRECT_URI'),
+        'client_id' => env('GOOGLE_CLIENT_ID'),
+        'client_secret' => env('GOOGLE_CLIENT_SECRET'),
+        'redirect' => env('GOOGLE_CALLBACK_URL'),
     ],
 
 ];
