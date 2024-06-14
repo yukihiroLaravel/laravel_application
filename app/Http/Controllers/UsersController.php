f182f598294df2a290689aafe937151733fc4264
eri shimizu <poire12@outlook.jp>
2024-06-14 16:42:02 +0900

    5章　いいね！機能・カラム追加

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -27,4 +27,17 @@ class UsersController extends Controller
         $deta += $this->userCounts($user);
         return View('Users.show', $deta);
     }
+
+    public function favorites($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->favorites()->paginate(9);
+        $data = [
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        
+        return view('users.show', $data);
+    }
 }
