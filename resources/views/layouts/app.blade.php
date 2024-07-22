0ad32e6afb6093042efd784e3814041cc764d94c
irochi1184 <ken.office.arita@gmail.com>
2024-07-23 07:07:49 +0900

    4章　ユーザ一覧、動画投稿・削除

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,9 +3,10 @@
 
 <head>
   <meta charset="utf-8">
-  <title>YouTubeまとめ×コミュニケーション</title>
+  <title>YouTubeまとめ×SNS</title>
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="{{ asset('/css/styles.css') }}">
 </head>
 
 <body>
