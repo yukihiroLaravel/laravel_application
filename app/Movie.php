003ad79e0c0e8ae7fb78013ff12ebf87eb216592
ookawa999jp <ookawa999jp@gmail.com>
2024-07-25 19:50:26 +0900

    環境調査でide-helperがモデルのメタ情報を外部ファイル出力できたのでモデルのメタ情報のコメントを削除した

--- app/Movie.php
+++ app/Movie.php
@@ -5,36 +5,6 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
-/**
- * App\Movie
- *
- * @property int $id
- * @property string|null $title
- * @property int $user_id
- * @property string $youtube_id
- * @property \Illuminate\Support\Carbon|null $created_at
- * @property \Illuminate\Support\Carbon|null $updated_at
- * @property string|null $deleted_at
- * @method static \Illuminate\Database\Eloquent\Builder|Movie newModelQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Movie newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|Movie query()
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereCreatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereDeletedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereTitle($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereUpdatedAt($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereUserId($value)
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereYoutubeId($value)
- * @mixin \Eloquent
- * @method static \Illuminate\Database\Query\Builder|Movie onlyTrashed()
- * @method static \Illuminate\Database\Query\Builder|Movie withTrashed()
- * @method static \Illuminate\Database\Query\Builder|Movie withoutTrashed()
- * @property-read \App\User $user
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\User[] $favoriteUsers
- * @property-read int|null $favorite_users_count
- * @property int $favorite_flag
- * @method static \Illuminate\Database\Eloquent\Builder|Movie whereFavoriteFlag($value)
- */
 class Movie extends Model
 {
     use SoftDeletes;
