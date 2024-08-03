23c8592c8fb14f16de3f765983dfc56a19da2bd7
yuji inomata <inoman360.ai@gmail.com>
2024-08-04 05:01:58 +0900

    4章 動画登録・ユーザ詳細

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -10,8 +10,6 @@
         @endif
             <div class="col-lg-4 mb-5">
                 <div class="movie text-left d-inline-block">
-                <div class="col-lg-4 mb-5">
-                <div class="movie text-left d-inline-block">
                     <a href="{{ route('user.show', $user->id) }}">＠{{ $user->name }}</a>
                     <div>
                         @if ($movie)
