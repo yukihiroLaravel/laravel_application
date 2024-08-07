d4648cb519bce24311a0f8af0e7395eb0673682e
kazuya takei <takei0831ka@gmail.com>
2024-08-07 17:37:37 +0900

    4章 動画登録・ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,11 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 Route::get('/', 'UsersController@index');
+// ユーザ
+Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
