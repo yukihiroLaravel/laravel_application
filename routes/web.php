32aee505a6f4aa14c462898cf074d9cece5c247a
take <panicdepanic@gmail.com>
2024-09-13 19:04:15 +0900

    3章　ユーザー登録機能

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,10 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/', 'UsersController@index');
+
+
