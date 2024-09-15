50873aa9fa02e19e7bd4ba112419a1071a9a7cd4
takeshi ogido <ogido04@gmail.com>
2024-09-15 21:34:39 +0900

    ４章動画マイグレーションとモデル

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
+// ユーザ新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+
+// トップページを表示させる
+Route::get('/', 'UsersController@index');
+
+// ログイン後
+Route::group(['middleware' => 'auth'], function () {
+    // 動画
+    Route::prefix('movies')->group(function () {
+        Route::get('create', 'MoviesController@create')->name('movie.create');
+        Route::post('', 'MoviesController@store')->name('movie.store');
+        Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+    });
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
