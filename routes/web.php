37582058a6c86f950d33166bb7317ccd32aade2e
katsuhiro sakurai <dream.of.no.1.player@gmail.com>
2024-09-18 19:41:56 +0900

    4章 動画登録・ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -21,7 +21,11 @@ Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
 
+// ユーザ
 Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
