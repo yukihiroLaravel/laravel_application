d0577f8e6cddb588edd0a75fd5a7bdea70afa909
takeshi ogido <ogido04@gmail.com>
2024-09-22 19:44:31 +0900

    4章　ユーザ一覧、動画投稿機能・削除

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -1,16 +1,11 @@
-<h2 class="mt-5 mb-5">チャンネル一覧</h2>
-<div class="movies row mt-5 text-center">
-    @foreach ($users as $user)
-    @php
-        $movie = $user->movies->last();
-    @endphp
+    <div class="movies row mt-5 text-center">
+    @foreach ($movies as $movie)
         @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
             </div>
             <div class="row text-center mt-3">
         @endif
             <div class="col-lg-4 mb-5">
                 <div class="movie text-left d-inline-block">
-                    ＠{{ $user->name }}
                     <div>
                         @if ($movie)
                             <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}" frameborder="0"></iframe>
@@ -25,7 +20,21 @@
                     </p>
                 </div>
             </div>
+
+            
+{{ $movies->links('pagination::bootstrap-4') }}
+<p>
+    @if (isset($movie->comment))
+        {{ $movie->comment }}
+    @endif
+</p> 
+    @if (Auth::id() === $movie->user_id)
+        <form method="POST" action="{{ route('movie.delete', $movie->id) }}">
+            @csrf
+            @method('DELETE')
+                <button type="submit" class="btn btn-danger">この動画を削除する</button>
+        </form>
+    @endif
+
     @endforeach
     </div>
-{{ $users->links('pagination::bootstrap-4') }}
-                            
