f11a9a0976ccb789c8a5b8fc54173f1b1cadb5f2
takeshi ogido <ogido04@gmail.com>
2024-10-04 16:14:19 +0900

    6章 fly.ioへのデプロイ

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -1,14 +1,11 @@
 <div class="movies row mt-5 text-center">
     @foreach ($movies as $movie)
         @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
-            <!-- 動画の一列目はここに入る -->
-                </div>
-                    <!-- 二列目以降はここに入る -->
-                            <div class="row text-center mt-3">
+        </div>
+            <div class="row text-center mt-3">
         @endif
         <div class="col-lg-4 mb-5">
             <div class="movie text-left d-inline-block">
-                <!-- 動画右上のいいね数 -->
                 @php
                     $countFavoriteUsers = $movie->favoriteUsers()->count();
                     $videoTitle="※動画が未登録です";
@@ -26,13 +23,9 @@
                             }
                         }
                 @endphp 
-                <!-- いいねの位置 -->
-
                 <div class="text-right mb-2">いいね！
                     <span class="badge badge-pill badge-success">{{ $countFavoriteUsers }}</span>
                 </div>    
-
-                    <!-- 動画 -->
                     <div>
                     @if ($movie)
                             <iframe width="290" height="163.125" src="{{ 'https://www.youtube.com/embed/'.$movie->youtube_id }}?controls=1&loop=1&playlist={{ $movie->youtube_id }}" frameborder="0"></iframe>
@@ -40,8 +33,6 @@
                             <iframe width="290" height="163.125" src="https://www.youtube.com/embed/" frameborder="0"></iframe>
                         @endif
                     </div>
-
-                    <!-- 動画のタイトル -->
                     <p>
                         @if (isset($movie->title))
                             {{ $movie->title }}
@@ -49,11 +40,7 @@
                             {{ $videoTitle }}
                         @endif
                     </p>
-                    
-                    <!-- お気に入りした動画が表示される -->
                     @include('favorite.favorite_button', ['movie' => $movie])
-
-                    <!-- ログインしてるユーザーが動画を所有しているユーザーのIDと同じだった場合 -->
                     @if (Auth::id() === $movie->user_id)
                         <div class="d-flex justify-content-between">
                             <form method="POST" action="{{ route('movie.delete', $movie->id) }}">
@@ -68,6 +55,4 @@
         </div>
     @endforeach
 </div>
-
-<!-- ページネーション -->
 {{ $movies->links('pagination::bootstrap-4') }}
