05ddf8aba2d9ea57ec251ee9bf56479b9b18c5e5
yuki tonomoto <yukinko_glay@yahoo.co.jp>
2024-10-08 16:02:16 +0900

    6章 RESTful API

--- resources/views/movies/movies.blade.php
+++ resources/views/movies/movies.blade.php
@@ -7,7 +7,21 @@
     <div class="col-lg-4 mb-5">
         <div class="movie text-left d-inline-block">
             @php
-            $countFavoriteUsers = $movie->favoriteUsers()->count();
+                $countFavoriteUsers = $movie->favoriteUsers()->count();
+                $videoTitle="※動画が未登録です";
+                if ($movie) {
+                $keyName = config('app.YouTubeDataApiKey');
+                $apiUrl = "https://www.googleapis.com/youtube/v3/videos?id={$movie->youtube_id}&key={$keyName}&part=snippet";
+                $jsonData = file_get_contents($apiUrl);
+                if ($jsonData) {
+                $decodedData = json_decode($jsonData, true);
+                if ($decodedData['pageInfo']['totalResults'] !== 0){
+                $videoTitle = $decodedData['items']['0']['snippet']['title'];
+                }
+                } else {
+                $videoTitle="※一時的な情報制限中です";
+                }
+                }
             @endphp
             <div class="text-right mb-2">いいね！
                 <span class="badge badge-pill badge-success">{{ $countFavoriteUsers }}</span>
@@ -22,6 +36,8 @@
             <p>
                 @if (isset($movie->title))
                 {{ $movie->title }}
+                @else
+                {{ $videoTitle }}
                 @endif
             </p>
             @include('favorite.favorite_button', ['movie' => $movie])
@@ -31,14 +47,14 @@
                 @endif
             </p>
             @if (Auth::id() === $movie->user_id)
-                <div class="d-flex justify-content-between">
-                    <form method="POST" action="{{ route('movie.delete', $movie->id) }}">
-                        @csrf
-                        @method('DELETE')
-                        <button type="submit" class="btn btn-danger">この動画を削除する</button>
-                    </form>
-                    <a href="{{ route('movie.edit', $movie->id) }}" class="btn btn-primary">編集する</a>
-                </div>
+            <div class="d-flex justify-content-between">
+                <form method="POST" action="{{ route('movie.delete', $movie->id) }}">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">この動画を削除する</button>
+                </form>
+                <a href="{{ route('movie.edit', $movie->id) }}" class="btn btn-primary">編集する</a>
+            </div>
             @endif
         </div>
     </div>
--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -2,12 +2,26 @@
 <div class="movies row mt-5 text-center">
     @foreach ($users as $user)
     @php
-    $movies = $user->movies()->get();
-    $totalFavorites = 0;
-    foreach ($movies as $movie){
-    $totalFavorites += $movie->favoriteUsers()->count();
-    }
-    $movie = $user->movies->last();
+        $movies = $user->movies()->get();
+        $totalFavorites = 0;
+        foreach ($movies as $movie){
+        $totalFavorites += $movie->favoriteUsers()->count();
+        }
+        $movie = $user->movies->last();
+        $videoTitle="※動画が未登録です";
+                if ($movie) {
+                    $keyName = config('app.YouTubeDataApiKey');
+                    $apiUrl = "https://www.googleapis.com/youtube/v3/videos?id={$movie->youtube_id}&key={$keyName}&part=snippet";
+                    $jsonData = file_get_contents($apiUrl);
+                    if ($jsonData) {
+                        $decodedData = json_decode($jsonData, true);
+                        if ($decodedData['pageInfo']['totalResults'] !== 0){
+                            $videoTitle = $decodedData['items']['0']['snippet']['title'];
+                        }
+                    } else {
+                        $videoTitle="※一時的な情報制限中です";
+                    }
+                }
     @endphp
     @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
 </div>
@@ -28,7 +42,9 @@
             </div>
             <p>
                 @if (isset($movie->title))
-                {{ $movie->title }}
+                    {{ $movie->title }}
+                @else
+                    {{ $videoTitle }}
                 @endif
             </p>
         </div>
