afd9191de7b8c09c032d134f0eedc3c209a2cb27
izumi sakamoto <zmskmt1999@gmail.com>
2024-10-13 22:02:48 +0900

    追加機能・検索機能

--- resources/views/users/users.blade.php
+++ resources/views/users/users.blade.php
@@ -1,5 +1,22 @@
-<h2 class="mt-5 mb-5">チャンネル一覧</h2>
-<div class="movies row mt-5 text-center">
+<div class="main" style="display: flex; justify-content: space-between;"> <!-- 追記 -->
+    <div class="contents1"> <!-- 追記 -->
+        <h2 class="mt-5 mb-5">チャンネル一覧</h2>
+        <!-- <div class="movies row mt-5 text-center"> 15行目に移動-->
+    </div>
+<!-- 追記ここから -->
+    <div class="contents2">
+        <div class="input-group" style="position: relative; top: 50px;">
+            <form action="{{ route('user.search') }}" method="post" style="display: flex; justify-content: right;">
+            @csrf
+            <input type="search" class="form-control rounded" name="search" value="{{ request('search') }}"placeholder="キーワードを入力" aria-label="検索..." aria-describedby="search-addon">
+            <!-- <input type="submit" value="検索" class="btn btn-info"> どちらか消す -->
+            <button type="submit" class="btn btn-outline-primary" data-mdb-ripple-init >➤</button>
+            </form>
+        </div>
+    </div>
+</div>
+<!-- 追記ここまで -->
+    <div class="movies row mt-5 text-center"> <!-- 4行目から移動 -->
     @foreach ($users as $user)
         @php
             $movies = $user->movies()->get();
@@ -10,7 +27,7 @@
             $movie = $user->movies->last();
         @endphp
         @if ($loop->iteration % 3 === 1 && $loop->iteration !== 1)
-            </div>
+    </div>
             <div class="row text-center mt-3">
         @endif
             <div class="col-lg-4 mb-5">
