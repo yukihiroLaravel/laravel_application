10eeb52904330baaa87e907177b5de880cbbf7c2
miyagi <yagi3885@gmail.com>
2024-11-19 20:55:18 +0900

    4章 ユーザ一覧、動画投稿・削除

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
+
+    /**
+     * 各ユーザが所有している動画を1つだけトップページに表示する
+     * 「ユーザ一覧」
+     */
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id', 'desc')->paginate(9);
+
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 }
