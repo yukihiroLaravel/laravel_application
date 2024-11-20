8395efe4010b0bebd1f4fea8f3e63e93044e0673
miyagi <yagi3885@gmail.com>
2024-11-20 10:13:23 +0900

    4章 動画登録・ユーザ詳細

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,9 @@
 */
 
 // ユーザ新規登録
+
+use App\Http\Controllers\UsersController;
+
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
@@ -20,7 +23,11 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
+// ユーザ
 Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 
 // ログイン後
 Route::group(['middleware' => 'auth'], function () {
