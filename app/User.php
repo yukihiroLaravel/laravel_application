d2d238ed804acdfe808da7ce7605ad890b2d533f
jin 6hara <job6hara@gmail.com>
2024-11-24 19:21:47 +0900

    6章laravelapp-6hara-4

--- app/User.php
+++ app/User.php
@@ -5,12 +5,12 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\softDeletes;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class User extends Authenticatable
 {
     use Notifiable;
-    use softDeletes;
+    use SoftDeletes;
 
     public function movies()
     {
