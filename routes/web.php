ed0b9f8825c1efa1f58506b3dc7da11f0d1aa311
masaaki tsuji <masaaki@example.com>
2024-12-04 07:37:28 +0900

    4章 動画登録・ユーザー詳細 訂正

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,11 @@
 */
 
 Route::get('/', 'UsersController@index');
+// ユーザ
+Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
 // ユーザ新規登録
 Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
 Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
