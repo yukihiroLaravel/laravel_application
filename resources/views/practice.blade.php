3f293e3ca27063557f3ab5ef4c493e3b4d7445ef
KzAshimo <good.ashimo@gmail.com>
2024-12-17 23:06:46 +0900

    3章ログイン処理

---
+++ resources/views/practice.blade.php
@@ -0,0 +1,3 @@
+@extends('commons.header');
+
+@extends('commons.footer');
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,20 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+use Illuminate\Support\Facades\Route;
+
+Route::get('/','UsersController@index');
+Route::get('/practice','PracticesController@index');
+
+//ユーザー登録
+Route::namespace('Auth')->group(function(){
+    Route::get('signup','RegisterController@showRegistrationForm')->name('signup');
+    Route::post('signup','RegisterController@register')->name('signup.post');
+});
+
+//ログイン
+Route::namespace('Auth')->group(function(){
+    Route::get('login','LoginController@showLoginForm')->name('login');
+    Route::post('login','LoginController@login')->name('login.post');
+    Route::get('logout','LoginController@logout')->name('logout');
 });
