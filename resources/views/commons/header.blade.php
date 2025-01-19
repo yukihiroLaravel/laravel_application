c2a2e38f81ba012c7aab7011f5db8ba5bebf807f
kosuke.chimata <chimatakousuke@gmail.com>
2025-01-20 00:21:16 +0900

    ３−３　ログイン機能

--- resources/views/commons/header.blade.php
+++ resources/views/commons/header.blade.php
@@ -1,4 +1,10 @@
-<div class="collapse navbar-collapse" id="nav-bar">
+<header class="mb-5">
+    <nav class="navbar navbar-expand-sm navbar-dark bg-dark">
+        <a class="navbar-brand" href="/">YouTubeまとめ<br>&ensp;×コミュニケーション</a>
+        <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#nav-bar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="nav-bar">
             <ul class="navbar-nav mr-auto"></ul>
             <ul class="navbar-nav">
                 @if (Auth::check())
