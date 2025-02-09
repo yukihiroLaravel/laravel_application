5a4a329c94df6b48fd8bd578ef953fa68a0bb709
森尾さんのmitsunobu morio <m.morio@rhythm.ocn.ne.jp>
2025-02-09 16:14:20 +0000

    chapter4 migration movie

--- database/migrations/2025_02_06_113627_create_movies_table.php
+++ database/migrations/2025_02_06_113627_create_movies_table.php
@@ -15,10 +15,16 @@ class CreateMoviesTable extends Migration
     {
         Schema::create('movies', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('youtube_id');
             $table->timestamps();
+            $table->softDeletes();
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
-
+    
     /**
      * Reverse the migrations.
      *
