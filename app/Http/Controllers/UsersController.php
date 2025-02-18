260a5adddae42544a61d571bb362fdf5027b69f0
Miko <mikodesu0415@gmail.com>
2025-02-18 22:37:09 +0900

    4章動画マイグレーションとモデル

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,11 +4,14 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UsersController extends Controller
+cclass UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 
 }
