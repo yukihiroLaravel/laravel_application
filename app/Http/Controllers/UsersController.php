17afd7f7baebae22fa991e370c443ed4b9d7a151
tatsunori-1550 <aba31209875cc@gmail.com>
2025-03-27 16:15:03 +0900

    3章トップページの表示

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -8,7 +8,7 @@ class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcom');
+        return view('welcome');
      
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,10 @@
 @extends('layouts.app')
 @section('content')
     <div class="center jumbotron bg-dark">
-        <div class="text-center text-whiate mt-2 pt-1">
+        <div class="text-center text-white mt-2 pt-1">
             <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>YouTubeまとめ</h1>
             <h1>× コミュニケーション</h1>
-    </div> 
+    </div> <!>
 </div>
-<h5 class="description text-center">みんなの"おすすめ動画を自由にシェアしよう</h5> 
+<h5 class="description text-center">みんなのおすすめ動画を自由にシェアしよう</h5> 
 @endsection  
