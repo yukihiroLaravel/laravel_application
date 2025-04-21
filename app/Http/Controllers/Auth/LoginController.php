e2c4b69a9926c2d8199aedb1c230aa2fae30bafe
tomomi yashiki <wagona3667@gmail.com>
2025-04-21 15:06:34 +0900

    3章　ログイン機能

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -26,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    protected $redirectTo = '/';   // リダイレクト先をトップページに変更
 
     /**
      * Create a new controller instance.
