40e2c8313e36ed0f59165202c2df21e705e12c1b
森尾さんのmitsunobu morio <m.morio@rhythm.ocn.ne.jp>
2025-05-01 07:14:40 +0000

    Chapter5 Like! New features and columns added

--- app/User.php
+++ app/User.php
@@ -1,45 +1,40 @@
 <?php
-
 namespace App;
-
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
-    use Notifiable;
-    use SoftDeletes;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast to native types.
-     *
-     * @var array
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-    
+    //(中略)
     public function movies()
     {
         return $this->hasMany(Movie::class);
+    }
+    public function favorites()
+    {
+        return $this->belongsToMany(Movie::class, 'favorites', 'user_id', 'movie_id')->withTimestamps();
+    }
+    public function favorite($movieId)
+    {
+        $exist = $this->isFavorite($movieId);
+        if ($exist) {
+            return false;
+        } else {
+            $this->favorites()->attach($movieId);
+            return true;
+        }
+    }
+    public function unfavorite($movieId)
+    {
+        $exist = $this->isFavorite($movieId);
+        if ($exist) {
+            $this->favorites()->detach($movieId);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public function isFavorite($movieId)
+    {
+        return $this->favorites()->where('movie_id', $movieId)->exists();
     }
 }
