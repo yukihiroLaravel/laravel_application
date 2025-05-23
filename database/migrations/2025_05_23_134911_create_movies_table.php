951063562a4d66dbf8bcd1547cb9446842989feb
hideki kishimoto <hidekishi_15109858986721@yahoo.co.jp>
2025-05-23 23:47:05 +0900

    4章 マイグレーションとモデル

---
+++ database/migrations/2025_05_23_134911_create_movies_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('title')->nullable();
+            $table->bigInteger('user_id')->unsigned()->index();
+            $table->string('youtube_id');
+            $table->timestamps();
+            $table->softDeletes();
+            // 外部キー制約
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+
+            //posts テーブルの user_id カラムは users テーブルの id カラムを参照する onDelete cascade は連動して削除する
+            //$table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
