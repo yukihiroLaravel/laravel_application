c2d27d62895bf981d478f380a22a5fa1255c5206
keishi morimura <morimurakeishi@gmail.com>
2025-06-30 21:16:06 +0900

    4章ユーザ一覧、動画投稿・削除

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -3,6 +3,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\User;
 use App\Movie;
+use App\Http\Requests\MovieRequest;
+
 class MoviesController extends Controller
 {
     public function create()
@@ -14,5 +16,24 @@ class MoviesController extends Controller
             'movies' => $movies,
         ];
         return view('movies.create', $data);
+    }
+
+    public function store(MovieRequest $request)
+    {
+        $movie = new Movie;
+        $movie->youtube_id = $request->youtube_id;
+        $movie->title = $request->title;
+        $movie->user_id = $request->user()->id;
+        $movie->save();
+        return back();
+    }
+
+    public function destroy($id)
+    {
+        $movie = Movie::findOrFail($id);
+        if (\Auth::id() === $movie->user_id) {
+            $movie->delete();
+        }
+        return back();
     }
 }
