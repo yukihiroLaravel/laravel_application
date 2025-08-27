8f0c3585c5d9e5e3de070061a1ab18bd2f9398dd
mohamad redzuan <mrmy162@gmail.com>
2025-08-27 20:11:41 +0900

    3章　ユーザ登録機能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class UserController extends Controller
+class UsersController extends Controller
 {
     public function index()
     {
