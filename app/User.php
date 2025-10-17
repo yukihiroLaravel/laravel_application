47327288496ec53e0917474ceee1b8a4b84d683e
4060nagafuchidaisuke <4060nagafuchidaisuke@gmail.com>
2025-10-17 10:35:38 +0000

    2章 データベース・マイグレーション・シーダー実装

--- app/User.php
+++ app/User.php
@@ -9,6 +9,7 @@ use Illuminate\Notifications\Notifiable;
 class User extends Authenticatable
 {
     use Notifiable;
+    use SoftDeletes; // 追記
 
     /**
      * The attributes that are mass assignable.
