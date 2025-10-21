19d427030ec007527a6cc0ea192cf0b595559e1e
Naomichi <lseqa.613@gmail.com>
2025-10-21 22:05:21 +0900

    3章 ログイン機能

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,23 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// 前半：条件＝こうしたら。。。
+//      →送信形式：見たい（Get） or 伝えたい（Post）or put or delete
+//      →URL：住所
+//      →Route::get('/',
+
+// 後半：実行内容＝こうなる！
+//      →ファイル名＠アクション（関数）：どのファイルのどんな結果？
+//       例）会えるのか、付き合えるのか、フラれるのか？
+//      →'UsersController@index');
+
+// ユーザ新規登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
+// ログイン
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout');
+
+Route::get('/', 'UsersController@index');
+
