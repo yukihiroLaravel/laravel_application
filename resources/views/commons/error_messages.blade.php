5ea72ac415c9c9a013dcbb543b5dfd061ce1453e
mitsuru tamaki <tera0087@yahoo.co.jp>
2025-11-03 15:25:43 +0900

    3章 ユーザ登録機能

--- resources/views/commons/error_messages.blade.php
+++ resources/views/commons/error_messages.blade.php
@@ -1,6 +1,6 @@
 @if (count($errors) > 0)
     <ul class="alert alert-danger" role="alert">
-        @foreach ($error->all() as $error)
+        @foreach ($errors->all() as $error)
             <li class="ml-4">{{ $error }}</li>
         @endforeach
     </ul>
