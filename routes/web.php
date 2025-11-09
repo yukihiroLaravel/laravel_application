5d04c8b50ca9c1c1a0ba99db17d455ca06751c63
masuzaki <nakelmg@icloud.com>
2025-11-09 18:48:29 +0900

    コンフリクト解消: stash pop

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,18 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
+// ユーザ
 Route::get('/', 'UsersController@index');
+Route::prefix('users')->group(function () {
+    Route::get('{id}', 'UsersController@show')->name('user.show');
+});
+
+// ログイン後   middleware（コントローラーに行く前に必ず通る） => auth　 authを通ることによってログインしてるか、してないのか判定している。していたら下を表示
+Route::group(['middleware' => 'auth'], function () {
+    // 動画　　prefixは省略の意味がある。なのでget,postの後のルートの所に本当だったら、'movies Create'となる
+    Route::prefix('movies')->group(function () {
+        Route::get('create', 'MoviesController@create')->name('movie.create');
+        Route::post('', 'MoviesController@store')->name('movie.store');
+        Route::delete('{id}', 'MoviesController@destroy')->name('movie.delete');
+    });
+});
