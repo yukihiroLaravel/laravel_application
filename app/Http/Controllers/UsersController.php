39374579aac4e8f2b87da89c0d37ab7d887118ba
ryo maruyama <ryo@example.com>
2025-11-11 00:07:37 +0900

    四章 ユーザー一覧、動画投稿機能・削除機能

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User; // 追記
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('welcome');
+        $users = User::orderBy('id','desc')->paginate(9);
+        return view('welcome', [
+            'users' => $users,
+        ]);
     }
 }
