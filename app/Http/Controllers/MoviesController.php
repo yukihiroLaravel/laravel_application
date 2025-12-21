78ea14f03a21fe7f6ccfbe1864eed1d7161ace86
masuzaki <nakelmg@icloud.com>
2025-12-21 15:58:22 +0900

    5章いいね！機能・カラム追加

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -20,6 +20,7 @@ class MoviesController extends Controller
 
         return view('movies.create', $data);
     }
+    
 
     public function store(MovieRequest $request)
     {
@@ -27,9 +28,11 @@ class MoviesController extends Controller
         $movie->youtube_id = $request->youtube_id;
         $movie->title = $request->title;
         $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0;
         $movie->save();
         return back();
     }
+
 
     public function destroy($id)
     {
@@ -37,6 +40,32 @@ class MoviesController extends Controller
         if (\Auth::id() === $movie->user_id) {
             $movie->delete();
         }
+        return back();
+    }
+
+
+    public function edit($id)
+    {
+        $user = \Auth::user();
+        $movie = Movie::findOrFail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data=[
+            'user' => $user,
+            'movie' => $movie,
+            'movies' => $movies,
+        ];
+        return view('movies.edit', $data);
+    }
+
+
+    public function update(MovieRequest $request, $id)
+    {
+        $movie = Movie::findOrFail($id);
+        $movie->youtube_id = $request->youtube_id;
+        $movie->title = $request->title;
+        $movie->user_id = $request->user()->id;
+        $movie->favorite_flag = $request->favorite_flag ? 1 : 0;
+        $movie->save();
         return back();
     }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,8 +23,23 @@ $users = User::orderBy('id','desc')->paginate(9);
             'user' => $user,
             'movies' => $movies,
         ];
+
         $data += $this->userCounts($user);
+
         return view('users.show',$data);
     }
+
+     public function favorites($id)
+    {
+        $user = User::findOrFail($id);
+        $movies = $user->favorites()->paginate(9);
+        $data=[
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show', $data);
+    }
+
 }
 
