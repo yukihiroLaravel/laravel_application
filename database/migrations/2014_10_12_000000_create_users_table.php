88e25d59c12c19a30189c04d167d20788eea212b
kazuho ishii <kazuho.ishii@gmail.com>
2025-12-29 01:33:56 +0900

    2章 データベース・マイグレーション・シーダー実装

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\SoftDeletes; // 追記
 
 class CreateUsersTable extends Migration
 {
@@ -21,6 +22,7 @@ class CreateUsersTable extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
