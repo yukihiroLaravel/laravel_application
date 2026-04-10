d6d5ad6797fd00ef37736766e80c6092019639f8
shinichi ushioda <kouya0315.riri0717@gmail.com>
2026-04-10 12:03:34 +0900

    4章 動画登録・ユーザ詳細

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,14 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    //ユーザが持っている動画数を表示させるためのメソッド ここで定義することで、他のコントローラーでも使用できるようになる
+    public function userCounts($user)
+    {
+        $countMovies = $user->movies()->count();
+        
+        return [
+            'countMovies' => $countMovies,
+        ];
+    }
 }
--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\User;
 
 class UsersController extends Controller
 {
     public function index(){
-        
-        return view('welcome');
+       //ユーザをidの降順（新しい順）で取得　第一引数はカラム名、第二引数は順番 
+       $users = User::orderBy('id', 'desc')->paginate(9);
+       //welcome.blade.phpにusersを渡す　第一引数はviewのファイル名、第二引数はviewに渡すデータを配列で指定
+       return view('welcome',[
+           'users' => $users,
+       ]);
+    }
+
+    public function show($id)
+    {
+        $user = User::findOrfail($id);
+        $movies = $user->movies()->orderBy('id', 'desc')->paginate(9);
+        $data = [
+            'user' => $user,
+            'movies' => $movies,
+        ];
+        $data += $this->userCounts($user);
+        return view('users.show', $data);
     }
 }
