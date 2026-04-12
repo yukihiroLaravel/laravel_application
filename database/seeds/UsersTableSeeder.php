fb0574e374d9b36e8fbdf63ec9dbe557b60b5cbd
hiroko.k <okojowebworks@gmail.com>
2026-04-12 17:33:10 +0900

    2章 データベース・マイグレーション・シーダー実装

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+use Illuminate\Database\Seeder;
+// use Illuminate\Support\Facades\DB;
+
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        for ($i = 0; $i < 12; $i++) {
+            DB::table('users')->insert([
+                'name' => 'test' . $i,
+                'email' => "test{$i}@test.com",
+                'password' => bcrypt("test. {$i}")
+            ]);
+        }
+
+        // DB::table('users')->insert([
+        //     'name' => 'test1',
+        //     'email' => 'test1@test.com',
+        //     'password' => bcrypt('test1')
+        // ]);
+        // DB::table('users')->insert([
+        //     'name' => 'test2',
+        //     'email' => 'test2@test.com',
+        //     'password' => bcrypt('test2')
+        // ]);
+        // DB::table('users')->insert([
+        //     'name' => 'test3',
+        //     'email' => 'test3@sample.com',
+        //     'password' => bcrypt('test3')
+        // ]);
+        // DB::table('users')->insert([
+        //     'name' => 'test4',
+        //     'email' => 'test4@test.com',
+        //     'password' => bcrypt('test4')
+        // ]);
+    }
+}
