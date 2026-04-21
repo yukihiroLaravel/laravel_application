9951ee5575034a1b0cf799c5e083d07332554eb5
hiroko.k <okojowebworks@gmail.com>
2026-04-21 19:12:40 +0900

    3章 ユーザー登録機能

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -29,7 +29,8 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = RouteServiceProvider::HOME;
+    // ログイン後のページ表示（トップページへリダイレクト）
+    protected $redirectTo = '/';
 
     /**
      * Create a new controller instance.
