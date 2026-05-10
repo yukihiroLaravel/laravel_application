ef81cf1ab6222a64a520d208bf20438b70640cc0
hiroko.k <okojowebworks@gmail.com>
2026-05-10 17:57:40 +0900

    3章 トップページの表示2

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UsersController extends Controller
+{
+    public function index()
+    {
+        // welcome.blade.php というviewファイルを返す
+        return view('welcome');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// getリクエストで [/(root)]にアクセスした場合、UsersControllerのindexメソッドを実行する
+Route::get('/', 'UsersController@index');
