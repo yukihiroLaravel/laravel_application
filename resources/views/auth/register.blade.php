b2799d922909c5bf5ac6fed8751fcdb4cbb34a87
naoki tawa <naokitawa10@gmail.com>
2026-08-16 23:36:11 +0900

    3章　ログイン機能

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,7 @@
     </div>   
     <div class="row mt-5 mb-5">
         <div class="col-sm-6 offset-sm-3">
-            <form method="post" action="{{ route('signup.post') }}">
+            <form method="POST" action="{{ route('signup.post') }}">
                 @csrf
                 <div class="form-group">
                     <label for="name">名前</label>
